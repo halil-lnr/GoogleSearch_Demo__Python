931b3f78369784bc4118868c7c31dedc0e6eda32
Halil Karimis <78182710+halil-lnr@users.noreply.github.com>
2023-11-28 14:23:04 -0600

    Update test-driver2.py

--- test-driver2.py
+++ test-driver2.py
@@ -1,7 +1,27 @@
-from selenium import webdriver
+# from selenium import webdriver
 
-url = "https://google.com/"
-driver = webdriver.Chrome()
+# url = "https://google.com/"
+# driver = webdriver.Chrome()
+
+import os
+
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+webdriver_service = Service("chromedriver")
+options = Options()
+options.headless = False
+options.accept_insecure_certs = True
+
+driver = webdriver.Chrome(service=webdriver_service, options=options)
+
 driver.get(url)
 print(" Google Page Loading... ")
 driver.maximize_window()

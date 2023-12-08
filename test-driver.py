4e1e1b41b91fd5b41e985bded3139b796cb97c7b
Halil Karimis <78182710+halil-lnr@users.noreply.github.com>
2023-12-07 23:27:05 -0600

    Update test-driver.py

--- test-driver.py
+++ test-driver.py
@@ -1,32 +1,27 @@
+# from selenium import webdriver
+
+# url = "https://google.com/"
+# driver = webdriver.Chrome()
 import os
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-base_url = "http://www.google.com"
+options = Options()
+options.headless = True
+options.accept_insecure_certs = True
 
+cwd = str(os.getcwd())
+chrome_driver = cwd + r'/chromedriver.exe'
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless=new")
-# options.add_argument("--headless")
-# options.add_argument("--window-size=1920x1080")
-options.add_argument('--allow-insecure-localhost')
-options.add_argument("--start-maximized")
-service = webdriver.ChromeService(executable_path="chromedriver.exe")        
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome(service=webdriver_service, options=options) 
+# driver = webdriver.Chrome(executable_path='/chromedriver.exe',options=options)
+url = "https://google.com/"
 
-# os.environ['WDM_LOG_LEVEL'] = '0'
-
-driver.implicitly_wait(20)
-
-driver.get(base_url)
+driver.get(url)
 print(" Google Page Loading... ")
 driver.maximize_window()
 
 page_title = driver.title
 print("Page title is : " + page_title)
-time.sleep(3)
-
-
